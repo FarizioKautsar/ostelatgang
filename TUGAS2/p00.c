96dffcdd223fa5ce99e58eb193238255ae4135ef
andrew tjondro <andrewtjondro00@gmail.com>
2020-06-24 02:34:06 +0700

    OS201 andrewtheodore

--- TUGAS2/p00.c
+++ TUGAS2/p00.c
@@ -79,18 +79,7 @@ void checkOpen(void) {
     }
 }
 
-void delay(int number_of_seconds) 
-{ 
-    // Converting time into milli_seconds 
-    int milli_seconds = 1000 * number_of_seconds; 
-  
-    // Storing start time 
-    clock_t start_time = clock(); 
-  
-    // looping till required time is not achieved 
-    while (clock() < start_time + milli_seconds) 
-        ; 
-} 
+
 int main(void) {
     sprintf(tmpStr, "START PID[%d] PPID[%d]", getpid(), getppid());
     myprint(akunGitHub, tmpStr);
@@ -129,6 +118,5 @@ int main(void) {
     // blah... blah... blah...
     // blah... blah... blah...
     myprint(akunGitHub, "BYEBYE =====  ===== =====");
-	delay(3);
 }
 
