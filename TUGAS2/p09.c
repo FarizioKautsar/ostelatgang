a22ca36f30741beffc4f843da104097ba02a276b
marcelvaldhano <60356482+marcelvaldhano@users.noreply.github.com>
2020-06-24 07:52:48 +0700

    Update p09.c

--- TUGAS2/p09.c
+++ TUGAS2/p09.c
@@ -8,7 +8,7 @@
 #define  akunGitHub "marcelvaldhano"
 #include "p00.h"
 
-char*    progs[]={P01, P02, P03, P04, P05, P06, P07, P08, P09, P10, P11;
+char*    progs[]={P01, P02, P03, P04, P05, P06, P07, P08, P09, P10, P11};
 myshare* mymap;
 
 int init(void) {
